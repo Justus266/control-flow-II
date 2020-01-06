40c1bdccfc08e833caac3f1d48e7a8080e6640c0
Justus <02justus.boesch@gulllakecs.org>
2020-01-06 05:38:04 -0800

    Add Running Totals

--- ControlFlow.py
+++ ControlFlow.py
@@ -2,5 +2,15 @@
 Programmer: Justus Boesch
 Date: 1.6.20
 Program: Running Total
+
+This Program asks users for five numbers
+It then sums up the numbers
 """
 
+sum = 0
+
+for i in range(5):
+    enter_a_number = int(input("Enter a number: "))
+    sum = sum + enter_a_number
+
+print("\nSum of your numbers is: " + str(sum))
