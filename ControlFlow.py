5f77efe587c174352586c46da43afff239d462cf
Justus266 <58734134+Justus266@users.noreply.github.com>
2020-01-07 08:43:30 -0500

    Average test scores

--- ControlFlow.py
+++ ControlFlow.py
@@ -16,3 +16,20 @@ for i in range(how_many_numbers):
     sum = sum + enter_a_number
 
 print("\nSum of your numbers is: " + str(sum))
+
+
+"""
+Programmer: Justus Boesch
+Date: 1.7.20
+Program: Average test scores
+
+This program asks users how many tests they wish to average
+"""
+
+total = 0
+how_many_tests = int(input("How many tests would you like to average?: "))
+print("")
+
+for i in range(how_many_tests):
+    enter_a_score = int(input("Enter a score: "))
+    total = total + enter_a_score
