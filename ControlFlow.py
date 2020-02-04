f77ec94ba763f0de783d5b5f950278d81aafd0ba
Justus <02justus.boesch@gulllakecs.org>
2020-02-04 04:48:27 -0800

    Functions & Variables = printSomething()

--- ControlFlow.py
+++ ControlFlow.py
@@ -1,16 +1,24 @@
 # 2.3.20
 #Greeting Function
 
+#Declare Global Variable here.....
+
 name = input("\nHello! What's your name?: ")
+x = 15
 
+#Create Functions Here.....
 
-
-#Create Functions Here
 def greeting():
     print("\nHi There, " + name + "!")
     print("Very nice to meet you " + name)
 
+def printSomething():
+    x = 3
+    print(x)
 
-#Call Functions Here
+
+#Call Functions Here.....
 
 greeting()
+printSomething()
+print(x)
